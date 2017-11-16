46d2a5bc13986dae18eda53c7e8e1bab1113c7c4
thoopring <thoopring@gmail.com>
2017-11-16 21:32:06 +0900

    Adict 20171116

--- SigServer/src/MainFrm.cpp
+++ SigServer/src/MainFrm.cpp
@@ -14,6 +14,7 @@ wzMQueue<int, SIG_COMMLOG>  SigCommLogQ;
 //---------------------------------------------------------------------------
 //  브랜치 테스트
 // 내저정소 먼저 Push
+// pushpush
 //---------------------------------------------------------------------------
 __fastcall TMainF::TMainF(TComponent* Owner)
 	: TForm(Owner)
